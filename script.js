099af80a45f6b9831c235dd266a07e0c59ca7ee3
Darshan6424 <subedidarshan64@gmail.com>
2024-09-11 23:00:41 +0545

    show output done for 2 questions

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function toggleCode(id) {
     const codeElement = document.getElementById(id);
     if (codeElement.style.display === "none" || codeElement.style.display === "") {
-        codeElement.style.display = "block"; // Show the code
+        codeElement.style.display = "block"; // it shows the code ig
     } else {
-        codeElement.style.display = "none"; // Hide the code
+        codeElement.style.display = "none"; // it hides the code ig
     }
 }
+
+function togglecode(id) {
+    const btn = document.getElementById(id);
+    if (btn.innerHTML.trim() === "See Code") { 
+        btn.innerHTML = "Hide Code";
+    } 
+    else {
+        btn.innerHTML = "See Code";
+    }
+}
+
+function toggleoutput(id) {
+    const btn = document.getElementById(id);
+    if (btn.innerHTML.trim() === "See Output") { 
+        btn.innerHTML = "Hide Output";
+    } 
+    else {
+        btn.innerHTML = "See Output";
+    }
+}
+
